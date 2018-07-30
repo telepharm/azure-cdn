519243a73cc4788e005e612f647d5fb5e3b02e84
cah-calebboyd <caleb.boyd@telepharm.com>
2018-07-30 14:44:47 -0500

    chore: bump dependencies

--- index.js
+++ index.js
@@ -1,22 +1,116 @@
-//inspired by:
-//https://github.com/bestander/deploy-azure-cdn
 'use strict';
 
-var _toConsumableArray = require('babel-runtime/helpers/to-consumable-array')['default'];
-
-var _Object$defineProperty = require('babel-runtime/core-js/object/define-property')['default'];
-
-var _regeneratorRuntime = require('babel-runtime/regenerator')['default'];
-
-var _Object$assign = require('babel-runtime/core-js/object/assign')['default'];
-
-var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
-
-_Object$defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-exports['default'] = upload;
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+var _toConsumableArray2 = require('babel-runtime/helpers/toConsumableArray');
+
+var _toConsumableArray3 = _interopRequireDefault(_toConsumableArray2);
+
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+//Erase all blobs under folder
+var eraseBlobsAsync = function () {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(service, container, folder, concurrency, test, log) {
+    var blobs;
+    return _regenerator2.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return service.listBlobsSegmentedWithPrefixAsync(container, folder, null);
+
+          case 2:
+            blobs = _context.sent[0];
+            _context.next = 5;
+            return map(blobs.entries, function (blob) {
+              return test ? log(`deleted* ${blob.name}`) : service.deleteBlobAsync(container, blob.name).then(function () {
+                return log(`deleted ${blob.name}`);
+              });
+            }, { concurrency });
+
+          case 5:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, this);
+  }));
+
+  return function eraseBlobsAsync(_x, _x2, _x3, _x4, _x5, _x6) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+//gzip fileName and return file name for smallest version
+
+
+var gzipAndCompareAsync = function () {
+  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(fileName, size) {
+    var gzip, tmp, file, out, writing, compressedSize;
+    return _regenerator2.default.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            gzip = _zlib2.default.createGzip({
+              level: 9
+            });
+            tmp = fileName + '.gz';
+            file = fs.createReadStream(fileName);
+            out = fs.createWriteStream(tmp);
+            writing = new _bluebird2.default(function (res, rej) {
+              out.once('close', res);
+              gzip.once('error', rej);
+            });
+
+            file.pipe(gzip).pipe(out);
+            _context2.next = 8;
+            return writing;
+
+          case 8:
+            _context2.next = 10;
+            return getFileSizeAsync(tmp);
+
+          case 10:
+            compressedSize = _context2.sent;
+
+            if (!(compressedSize > size)) {
+              _context2.next = 15;
+              break;
+            }
+
+            _context2.next = 14;
+            return fs.unlinkAsync(tmp);
+
+          case 14:
+            return _context2.abrupt('return', fileName);
+
+          case 15:
+            return _context2.abrupt('return', tmp);
+
+          case 16:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, this);
+  }));
+
+  return function gzipAndCompareAsync(_x7, _x8) {
+    return _ref2.apply(this, arguments);
+  };
+}();
 
 var _azureStorage = require('azure-storage');
 
@@ -42,10 +136,14 @@ var _bluebird = require('bluebird');
 
 var _bluebird2 = _interopRequireDefault(_bluebird);
 
-var fs = _bluebird2['default'].promisifyAll(_fs2['default']);
-_bluebird2['default'].promisifyAll(_azureStorage2['default'].BlobService.prototype);
-var LinearRetryPolicyFilter = _azureStorage2['default'].LinearRetryPolicyFilter;
-var map = _bluebird2['default'].map;
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//inspired by:
+//https://github.com/bestander/deploy-azure-cdn
+var fs = _bluebird2.default.promisifyAll(_fs2.default);
+_bluebird2.default.promisifyAll(_azureStorage2.default.BlobService.prototype);
+var LinearRetryPolicyFilter = _azureStorage2.default.LinearRetryPolicyFilter;
+var map = _bluebird2.default.map;
 
 //
 function getFileSizeAsync(path) {
@@ -53,218 +151,160 @@ function getFileSizeAsync(path) {
     return !stat.isDirectory() && stat.size;
   });
 }
-
-//Erase all blobs under folder
-function eraseBlobsAsync(service, container, folder, concurrency, test, log) {
-  var blobs;
-  return _regeneratorRuntime.async(function eraseBlobsAsync$(context$1$0) {
-    while (1) switch (context$1$0.prev = context$1$0.next) {
-      case 0:
-        context$1$0.next = 2;
-        return _regeneratorRuntime.awrap(service.listBlobsSegmentedWithPrefixAsync(container, folder, null));
-
-      case 2:
-        blobs = context$1$0.sent[0];
-        context$1$0.next = 5;
-        return _regeneratorRuntime.awrap(map(blobs.entries, function (blob) {
-          return test ? log('deleted* ' + blob.name) : service.deleteBlobAsync(container, blob.name).then(function () {
-            return log('deleted ' + blob.name);
-          });
-        }, { concurrency: concurrency }));
-
-      case 5:
-      case 'end':
-        return context$1$0.stop();
-    }
-  }, null, this);
-}
-
-//gzip fileName and return file name for smallest version
-function gzipAndCompareAsync(fileName, size) {
-  var gzip, tmp, file, out, writing, compressedSize;
-  return _regeneratorRuntime.async(function gzipAndCompareAsync$(context$1$0) {
-    while (1) switch (context$1$0.prev = context$1$0.next) {
-      case 0:
-        gzip = _zlib2['default'].createGzip({
-          level: 9
-        });
-        tmp = fileName + '.gz';
-        file = fs.createReadStream(fileName);
-        out = fs.createWriteStream(tmp);
-        writing = new _bluebird2['default'](function (res, rej) {
-          out.once('close', res);
-          gzip.once('error', rej);
-        });
-
-        file.pipe(gzip).pipe(out);
-        context$1$0.next = 8;
-        return _regeneratorRuntime.awrap(writing);
-
-      case 8:
-        context$1$0.next = 10;
-        return _regeneratorRuntime.awrap(getFileSizeAsync(tmp));
-
-      case 10:
-        compressedSize = context$1$0.sent;
-
-        if (!(compressedSize > size)) {
-          context$1$0.next = 15;
-          break;
-        }
-
-        context$1$0.next = 14;
-        return _regeneratorRuntime.awrap(fs.unlinkAsync(tmp));
-
-      case 14:
-        return context$1$0.abrupt('return', fileName);
-
-      case 15:
-        return context$1$0.abrupt('return', tmp);
-
-      case 16:
-      case 'end':
-        return context$1$0.stop();
-    }
-  }, null, this);
-}
-
-function upload(_ref) {
-  var _ref$files = _ref.files;
-  var files = _ref$files === undefined ? [] : _ref$files;
-  var _ref$log = _ref.log;
-  var log = _ref$log === undefined ? function () {} : _ref$log;
-  var _ref$blobService = _ref.blobService;
-  var blobService = _ref$blobService === undefined ? [] : _ref$blobService;
-  var _ref$container = _ref.container;
-  var container = _ref$container === undefined ? null : _ref$container;
-  var _ref$containerOptions = _ref.containerOptions;
-  var containerOptions = _ref$containerOptions === undefined ? { publicAccessLevel: 'blob' } : _ref$containerOptions;
-  var _ref$folder = _ref.folder;
-  var folder = _ref$folder === undefined ? '' : _ref$folder;
-  var _ref$erase = _ref.erase;
-  var erase = _ref$erase === undefined ? true : _ref$erase;
-  var _ref$concurrency = _ref.concurrency;
-  var concurrency = _ref$concurrency === undefined ? 10 : _ref$concurrency;
-  var _ref$zip = _ref.zip;
-  var zip = _ref$zip === undefined ? false : _ref$zip;
-  var _ref$metadata = _ref.metadata;
-  var metadata = _ref$metadata === undefined ? { cacheControl: 'public, max-age=31556926' } : _ref$metadata;
-  var _ref$test = _ref.test;
-  var test = _ref$test === undefined ? false : _ref$test;
-  var service;
-  return _regeneratorRuntime.async(function upload$(context$1$0) {
-    while (1) switch (context$1$0.prev = context$1$0.next) {
-      case 0:
-        if (container) {
-          context$1$0.next = 2;
-          break;
-        }
-
-        throw new Error('Usage error: container must be set to the container name');
-
-      case 2:
-        service = _azureStorage2['default'].createBlobService.apply(_azureStorage2['default'], _toConsumableArray(blobService)).withFilter(new LinearRetryPolicyFilter(100, 5));
-        context$1$0.next = 5;
-        return _regeneratorRuntime.awrap(service.createContainerIfNotExistsAsync(container, containerOptions));
-
-      case 5:
-        log('Processing ' + files.length + ' files (' + concurrency + ' concurrently).');
-
-        if (!erase) {
-          context$1$0.next = 9;
-          break;
-        }
-
-        context$1$0.next = 9;
-        return _regeneratorRuntime.awrap(eraseBlobsAsync(service, container, folder, concurrency, test, log));
-
-      case 9:
-        if (files.length) {
-          context$1$0.next = 11;
-          break;
-        }
-
-        return context$1$0.abrupt('return');
-
-      case 11:
-        return context$1$0.abrupt('return', map(files, function processFileAsync(file) {
-          var fileName, zipped, remoteFileName, meta, size;
-          return _regeneratorRuntime.async(function processFileAsync$(context$2$0) {
-            while (1) switch (context$2$0.prev = context$2$0.next) {
-              case 0:
-                fileName = file.path;
-                zipped = false;
-                remoteFileName = _path2['default'].join(folder, _path2['default'].relative(file.cwd, file.path));
-                meta = _Object$assign({}, metadata);
-                context$2$0.next = 6;
-                return _regeneratorRuntime.awrap(getFileSizeAsync(fileName));
-
-              case 6:
-                size = context$2$0.sent;
-
-                if (size) {
-                  context$2$0.next = 9;
-                  break;
-                }
-
-                return context$2$0.abrupt('return');
-
-              case 9:
-                meta.contentType = _mime2['default'].lookup(fileName);
-
-                if (!zip) {
-                  context$2$0.next = 14;
-                  break;
-                }
-
-                context$2$0.next = 13;
-                return _regeneratorRuntime.awrap(gzipAndCompareAsync(fileName, size));
-
-              case 13:
-                fileName = context$2$0.sent;
-
-              case 14:
-                if (fileName !== file.path) {
-                  zipped = true;
-                  meta.contentEncoding = 'gzip';
-                }
-                log('Uploading ' + remoteFileName);
-
-                if (test) {
-                  context$2$0.next = 19;
-                  break;
-                }
-
-                context$2$0.next = 19;
-                return _regeneratorRuntime.awrap(service.createBlockBlobFromLocalFileAsync(container, remoteFileName, fileName, meta)['finally'](function () {
-                  return zipped && fs.unlinkAsync(fileName);
-                }));
-
-              case 19:
-                if (!(test && zipped)) {
-                  context$2$0.next = 22;
-                  break;
-                }
-
-                context$2$0.next = 22;
-                return _regeneratorRuntime.awrap(fs.unlinkAsync(fileName));
-
-              case 22:
-                log('Uploaded ' + remoteFileName);
-
-              case 23:
-              case 'end':
-                return context$2$0.stop();
+exports.default = function () {
+  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee4(_ref3) {
+    var _ref3$files = _ref3.files,
+        files = _ref3$files === undefined ? [] : _ref3$files,
+        _ref3$log = _ref3.log,
+        log = _ref3$log === undefined ? function () {} : _ref3$log,
+        _ref3$blobService = _ref3.blobService,
+        blobService = _ref3$blobService === undefined ? [] : _ref3$blobService,
+        _ref3$container = _ref3.container,
+        container = _ref3$container === undefined ? null : _ref3$container,
+        _ref3$containerOption = _ref3.containerOptions,
+        containerOptions = _ref3$containerOption === undefined ? { publicAccessLevel: 'blob' } : _ref3$containerOption,
+        _ref3$folder = _ref3.folder,
+        folder = _ref3$folder === undefined ? '' : _ref3$folder,
+        _ref3$erase = _ref3.erase,
+        erase = _ref3$erase === undefined ? true : _ref3$erase,
+        _ref3$concurrency = _ref3.concurrency,
+        concurrency = _ref3$concurrency === undefined ? 10 : _ref3$concurrency,
+        _ref3$zip = _ref3.zip,
+        zip = _ref3$zip === undefined ? false : _ref3$zip,
+        _ref3$metadata = _ref3.metadata,
+        metadata = _ref3$metadata === undefined ? { cacheControl: 'public, max-age=31556926' } : _ref3$metadata,
+        _ref3$test = _ref3.test,
+        test = _ref3$test === undefined ? false : _ref3$test;
+    var service;
+    return _regenerator2.default.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            if (container) {
+              _context4.next = 2;
+              break;
             }
-          }, null, this);
-        }, { concurrency: concurrency }));
 
-      case 12:
-      case 'end':
-        return context$1$0.stop();
-    }
-  }, null, this);
-}
+            throw new Error('Usage error: container must be set to the container name');
 
-module.exports = exports['default'];
-// always unlink tmp file
+          case 2:
+            service = _azureStorage2.default.createBlobService.apply(_azureStorage2.default, (0, _toConsumableArray3.default)(blobService)).withFilter(new LinearRetryPolicyFilter(100, 5));
+            _context4.next = 5;
+            return service.createContainerIfNotExistsAsync(container, containerOptions);
+
+          case 5:
+            log(`Processing ${files.length} files (${concurrency} concurrently).`);
+
+            if (!erase) {
+              _context4.next = 9;
+              break;
+            }
+
+            _context4.next = 9;
+            return eraseBlobsAsync(service, container, folder, concurrency, test, log);
+
+          case 9:
+            if (files.length) {
+              _context4.next = 11;
+              break;
+            }
+
+            return _context4.abrupt('return');
+
+          case 11:
+            return _context4.abrupt('return', map(files, function () {
+              var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(file) {
+                var fileName, zipped, remoteFileName, meta, size;
+                return _regenerator2.default.wrap(function _callee3$(_context3) {
+                  while (1) {
+                    switch (_context3.prev = _context3.next) {
+                      case 0:
+                        fileName = file.path;
+                        zipped = false;
+                        remoteFileName = _path2.default.join(folder, _path2.default.relative(file.cwd, file.path));
+                        meta = (0, _assign2.default)({}, metadata);
+                        _context3.next = 6;
+                        return getFileSizeAsync(fileName);
+
+                      case 6:
+                        size = _context3.sent;
+
+                        if (size) {
+                          _context3.next = 9;
+                          break;
+                        }
+
+                        return _context3.abrupt('return');
+
+                      case 9:
+                        meta.contentType = _mime2.default.lookup(fileName);
+
+                        if (!zip) {
+                          _context3.next = 14;
+                          break;
+                        }
+
+                        _context3.next = 13;
+                        return gzipAndCompareAsync(fileName, size);
+
+                      case 13:
+                        fileName = _context3.sent;
+
+                      case 14:
+                        if (fileName !== file.path) {
+                          zipped = true;
+                          meta.contentEncoding = 'gzip';
+                        }
+                        log(`Uploading ${remoteFileName}`);
+
+                        if (test) {
+                          _context3.next = 19;
+                          break;
+                        }
+
+                        _context3.next = 19;
+                        return service.createBlockBlobFromLocalFileAsync(container, remoteFileName, fileName, meta).finally(function () {
+                          return zipped && fs.unlinkAsync(fileName);
+                        });
+
+                      case 19:
+                        if (!(test && zipped)) {
+                          _context3.next = 22;
+                          break;
+                        }
+
+                        _context3.next = 22;
+                        return fs.unlinkAsync(fileName);
+
+                      case 22:
+                        log(`Uploaded ${remoteFileName}`);
+
+                      case 23:
+                      case 'end':
+                        return _context3.stop();
+                    }
+                  }
+                }, _callee3, this);
+              }));
+
+              function processFileAsync(_x10) {
+                return _ref5.apply(this, arguments);
+              }
+
+              return processFileAsync;
+            }(), { concurrency }));
+
+          case 12:
+          case 'end':
+            return _context4.stop();
+        }
+      }
+    }, _callee4, this);
+  }));
+
+  function upload(_x9) {
+    return _ref4.apply(this, arguments);
+  }
+
+  return upload;
+}();
